29114064c261f580033b678c3c250757c96d058c
Leon Matthes <leon@matthes.biz>
2019-01-27 00:17:05 +0100

    Started adding Painting

--- LucidNightmare/Assets/Painting.cs
+++ LucidNightmare/Assets/Painting.cs
@@ -15,6 +15,17 @@ public class Painting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        alphaThreshold -= Time.deltaTime * 0.1f;
+        Material material =
+        GetComponent<MeshRenderer>().materials[0];
+        material.SetFloat("_Cutoff", alphaThreshold);
+        /*material.SetOverrideTag("RenderType", "TransparentCutout");
+        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+        material.SetInt("_ZWrite", 1);
+        material.EnableKeyword("_ALPHATEST_ON");
+        material.DisableKeyword("_ALPHABLEND_ON");
+        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        material.renderQueue = 2450;*/
     }
 }
