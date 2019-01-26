fe473d33aa98316254ec5f99361bde7aaf28d83b
BryanPrintz <Bryan.t@web.de>
2019-01-26 11:18:42 +0100

    Added Materials and player Move Script

--- LucidNightmare/Assets/Player/Movement.cs
+++ LucidNightmare/Assets/Player/Movement.cs
@@ -8,15 +8,18 @@ public class Movement : MonoBehaviour
 
     private Rigidbody rb;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
     }
 }
