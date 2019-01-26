0b0d6cc26f1076669c0fb2370b6761c14740890a
Leon Matthes <leon@matthes.biz>
2019-01-26 22:35:31 +0100

    Refactor Enemy path

---
+++ LucidNightmare/Assets/Painting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painting : MonoBehaviour
+{
+    float alphaThreshold = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
