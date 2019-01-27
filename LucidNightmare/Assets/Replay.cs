b5a18203855db34985bb5c3a7095a6caac12949d
BryanPrintz <Bryan.t@web.de>
2019-01-27 14:39:37 +0100

    Goldmaster

---
+++ LucidNightmare/Assets/Replay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Replay : MonoBehaviour
+{
+    public AudioClip Clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("RestartGame", 10.0f, float.PositiveInfinity);
+        //GetComponent<AudioSource>().PlayOneShot(Clip);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void RestartGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+}
