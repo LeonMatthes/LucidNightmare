fbd915456ec2b139ace3a3030fc6f5620eac9874
Leon Matthes <leon@matthes.biz>
2019-01-27 01:00:41 +0100

    small changes

--- LucidNightmare/Assets/Painting.cs
+++ LucidNightmare/Assets/Painting.cs
@@ -4,28 +4,22 @@ using UnityEngine;
 
 public class Painting : MonoBehaviour
 {
+    public Movement player;
+
     float alphaThreshold = 1;
+
+    private Material material;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        material = GetComponent<MeshRenderer>().materials[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        alphaThreshold -= Time.deltaTime * 0.1f;
-        Material material =
-        GetComponent<MeshRenderer>().materials[0];
+        //alphaThreshold = player.;
         material.SetFloat("_Cutoff", alphaThreshold);
-        /*material.SetOverrideTag("RenderType", "TransparentCutout");
-        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-        material.SetInt("_ZWrite", 1);
-        material.EnableKeyword("_ALPHATEST_ON");
-        material.DisableKeyword("_ALPHABLEND_ON");
-        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        material.renderQueue = 2450;*/
     }
 }
